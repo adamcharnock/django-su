170c7f693f9aca3d3d99f007462b50460461b02f
Pablo Martín <goinnn@gmail.com>
2011-12-03 13:58:21 +0100

    pep-8 and pyflakes refactor

--- src/django_su/urls.py
+++ src/django_su/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
 
-from django_su.views import login_as_user, su_exit
 
-urlpatterns = patterns("",
-    url(r"^su/(?P<user_id>[\d]+)/$", login_as_user, name="login_as_user"),
-    url(r"^su/$", su_exit, name="su_exit"),
+urlpatterns = patterns("django_su.views",
+    url(r"^su/(?P<user_id>[\d]+)/$", "login_as_user", name="login_as_user"),
+    url(r"^su/$", "su_exit", name="su_exit"),
 )
--- src/django_su/views.py
+++ src/django_su/views.py
@@ -1,38 +1,28 @@
 
-from django.http import HttpResponseForbidden, HttpResponseBadRequest, HttpResponseRedirect
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.conf import settings
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import login
 
-from django_su import SECRET_PASSWORD
 
 @user_passes_test(lambda u: u.is_staff)
 def login_as_user(request, user_id):
-    
     su_user = get_object_or_404(User, pk=user_id)
-    
     exit_user_pk = request.user.pk
     su_user.backend = settings.AUTHENTICATION_BACKENDS[0]
-    
     login(request, su_user)
-    
     request.session["exit_user_pk"] = exit_user_pk
-    
     return HttpResponseRedirect("/")
+
 
 def su_exit(request):
-    
     exit_user_pk = request.session.get("exit_user_pk", default=None)
-    
     if not exit_user_pk:
-        return HttpResponseBadRequest("This session was not su'ed into. Cannot exit.")
-    
+        return HttpResponseBadRequest(("This session was not su'ed into."
+                                       "Cannot exit."))
     staff_user = User.objects.get(pk=exit_user_pk)
     staff_user.backend = settings.AUTHENTICATION_BACKENDS[0]
-    
     login(request, staff_user)
-    
     return HttpResponseRedirect("/")
-
