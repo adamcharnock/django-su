2f5e9864d10508e4ad1aca4537eba45497861429
Pablo Martín <goinnn@gmail.com>
2011-12-03 17:00:29 +0100

    Grrr I forgot use "commit -a"

--- src/django_su/urls.py
+++ src/django_su/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns("django_su.views",
     url(r"^$", "su_exit", name="su_exit"),
+    url(r"^login/$", "login_as_user_form", name="login_as_user_form"),
     url(r"^(?P<user_id>[\d]+)/$", "login_as_user", name="login_as_user"),
 )
--- src/django_su/views.py
+++ src/django_su/views.py
@@ -3,10 +3,14 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+
+from django_su.forms import UserSuForm
+from django_su.utils import can_su_login
 
 
-@user_passes_test(lambda u: u.is_staff)
+@user_passes_test(can_su_login)
 def login_as_user(request, user_id):
     su_user = get_object_or_404(User, pk=user_id)
     exit_user_pk = request.user.pk
@@ -14,6 +18,20 @@ def login_as_user(request, user_id):
     login(request, su_user)
     request.session["exit_user_pk"] = exit_user_pk
     return HttpResponseRedirect(getattr(settings, "SU_REDIRECT_LOGIN", "/"))
+
+
+@user_passes_test(can_su_login)
+def login_as_user_form(request, user_form=UserSuForm):
+    data = None
+    if request.method == 'POST':
+        data = request.POST
+    form = user_form(data=data)
+    if form.is_valid():
+        user = form.get_user()
+        return login_as_user(request, user.pk)
+    return render_to_response('su/login.html',
+                              {'form': form},
+                              context_instance=RequestContext(request))
 
 
 def su_exit(request):
