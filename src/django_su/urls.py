a4f79614f8e7f4a58acc82a318b415720a700f52
Joe Vanderstelt <thisboyiscrazy@gmail.com>
2013-04-25 14:29:42 -0400

    better handling of auth backend.

--- src/django_su/urls.py
+++ src/django_su/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns("django_su.views",
     url(r"^$", "su_exit", name="su_exit"),
     url(r"^login/$", "su_login", name="su_login"),
-    url(r"^logout/$", "su_logout", name="su_logout"),
     url(r"^(?P<user_id>[\d]+)/$", "login_as_user", name="login_as_user"),
 )
