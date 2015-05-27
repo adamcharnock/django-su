4d581e664533a8e07fe65f577f4bddd9e17a8724
bashu <basil.shubin@gmail.com>
2015-05-27 20:11:20 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
-import os.path
-import sys
+import os
 from setuptools import setup, find_packages
 
-from django_su import __version__
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+from tz_detect import __version__
 
 setup(
     name='django-su',
     version=__version__,
-    description="Login as any user from the Django admin interface, then switch back when done",
-    long_description=open('README.rst').read() if os.path.exists("README.rst") else "",
-    author="Adam Charnock",
-    author_email="adam@adamcharnock.com",
-    url="https://github.com/adamcharnock/django-su",
-    license="MIT",
-
-    install_requires=["django"],
-
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Security",
-        "Framework :: Django",
-    ],
-
-    packages=find_packages(),
+    packages=find_packages(exclude=['example']),
     include_package_data=True,
+    license='MIT License',
+    description="Login as any user from the Django admin interface, then switch back when done",
+    long_description=README,
+    url='http://github.com/adamcharnock/django-su',
+    author='Adam Charnock',
+    author_email='adam@adamcharnock.com',
+    install_requires=[
+        'django>=1.4',
+    ],    
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
     zip_safe=False,
 )
