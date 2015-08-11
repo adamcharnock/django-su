aa3a6dd01d7681f92d1be42fb2831126ced7a76e
Basil Shubin <basil.shubin@gmail.com>
2015-08-11 14:41:10 +0500

    Update __init__.py

--- django_su/__init__.py
+++ django_su/__init__.py
@@ -2,5 +2,6 @@ import os
 
 # The fake password we will use to authenticate su'ed users
 SECRET_PASSWORD = os.urandom(64)
+VERSION = (0, 4, 8)
 
-__version__ = '0.4.8'
+__version__ = '.'.join([str(n) for n in VERSION])
