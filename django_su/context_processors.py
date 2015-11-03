db09411c7ba877e86ec80a26a6dcd03e01d2f3c6
bashu <basil.shubin@gmail.com>
2015-11-03 11:05:30 +0500

    clean up ``is_su`` context processor / snippet

--- django_su/context_processors.py
+++ django_su/context_processors.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def is_su(request):
-    exit_users_pk = request.session.get("exit_users_pk", default=[])
-    return {'DJANGO_SU_IS_SU': len(exit_users_pk) > 0}
+    return {
+        'IS_SU': len(request.session.get("exit_users_pk", default=[]))
+    }
