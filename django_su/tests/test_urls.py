7b2d58b751435fdfe8a90bb0b2f6367751133056
bashu <basil.shubin@gmail.com>
2015-12-17 09:41:12 +0500

    fixed "RemovedInDjango110Warning: django.conf.urls.patterns() is deprecated..."

--- django_su/tests/test_urls.py
+++ django_su/tests/test_urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^su/', include('django_su.urls')),
-)
+]
--- django_su/urls.py
+++ django_su/urls.py
@@ -5,9 +5,10 @@ try:
 except ImportError:  # django < 1.4
     from django.conf.urls.defaults import patterns, url
 
+from .views import su_logout, su_login, login_as_user
 
-urlpatterns = patterns("django_su.views",
-    url(r"^$", "su_logout", name="su_logout"),
-    url(r"^login/$", "su_login", name="su_login"),
-    url(r"^(?P<user_id>-?[\d]+)/$", "login_as_user", name="login_as_user"),
-)
+urlpatterns = [
+    url(r"^$", su_logout, name="su_logout"),
+    url(r"^login/$", su_login, name="su_login"),
+    url(r"^(?P<user_id>-?[\d]+)/$", login_as_user, name="login_as_user"),
+]
