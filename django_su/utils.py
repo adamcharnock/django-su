245700d9fc38dd7214e5c519284c0174a02ec4dd
bashu <basil.shubin@gmail.com>
2016-03-09 12:27:27 +0500

    using django's module loading utils, fix #45

--- django_su/utils.py
+++ django_su/utils.py
@@ -4,40 +4,8 @@ import warnings
 import collections
 
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 
-
-def importpath(path, error_text=None):
-    """
-    Import value by specified ``path``.
-    Value can represent module, class, object, attribute or method.
-    If ``error_text`` is not None and import will
-    raise ImproperlyConfigured with user friendly text.
-    """
-    result = None
-    attrs = []
-    parts = path.split('.')
-    exception = None
-    while parts:
-        try:
-            result = __import__('.'.join(parts), {}, {}, [''])
-        except ImportError as e:
-            if exception is None:
-                exception = e
-            attrs = parts[-1:] + attrs
-            parts = parts[:-1]
-        else:
-            break
-    for attr in attrs:
-        try:
-            result = getattr(result, attr)
-        except (AttributeError, ValueError) as e:
-            if error_text is not None:
-                raise ImproperlyConfigured('Error: %s can import "%s"' % (
-                    error_text, path))
-            else:
-                raise exception
-    return result
+from .compat import import_by_path as import_string
 
 
 def su_login_callback(user):
@@ -50,7 +18,7 @@ def su_login_callback(user):
     func = getattr(settings, 'SU_LOGIN_CALLBACK', None)
     if func is not None:
         if not isinstance(func, collections.Callable):
-            func = importpath(func)
+            func = import_string(func)
         return func(user)
     return user.has_perm('auth.change_user')
 
@@ -61,7 +29,7 @@ def custom_login_action(request, user):
         return False
 
     if not isinstance(func, collections.Callable):
-        func = importpath(func)
+        func = import_string(func)
     func(request, user)
 
     return True
