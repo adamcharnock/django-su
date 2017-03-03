2bc30f43f02781dfe312352050fe52bb347fd30c
Basil Shubin <basil.shubin@gmail.com>
2017-03-03 09:40:47 +0700

    added missing TEMPLATE_DIRS

--- test_settings.py
+++ test_settings.py
@@ -7,12 +7,14 @@ DATABASES = {
     }
 }
 
+TEMPLATE_DIRS = [
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_templates'),
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_templates'),
-        ],
+        'DIRS': TEMPLATE_DIRS,
         'APP_DIRS': True,
     },
 ]
