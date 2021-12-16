ebd84c86e07ec2ea683ee94f636e8876c30a4ba9
Petr Dlouhý <petr.dlouhy@email.cz>
2021-12-16 14:34:33 +0100

    fixes for Django 4.0

--- django_su/forms.py
+++ django_su/forms.py
@@ -3,7 +3,7 @@
 from django import forms
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
--- django_su/urls.py
+++ django_su/urls.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-try:
-    from django.conf.urls import url
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import url
+from django.urls import path
 
 from .views import su_logout, su_login, login_as_user
 
 urlpatterns = [
-    url(r"^$", su_logout, name="su_logout"),
-    url(r"^login/$", su_login, name="su_login"),
-    url(r"^(?P<user_id>-?[\d]+)/$", login_as_user, name="login_as_user"),
+    path("", su_logout, name="su_logout"),
+    path("login/", su_login, name="su_login"),
+    path("<int:user_id>/", login_as_user, name="login_as_user"),
 ]
