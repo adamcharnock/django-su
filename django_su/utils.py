1e35740c6c7cc28457767b13f065f1dcfc4b81fc
Julian <juliandgoddard@gmail.com>
2023-10-03 13:07:56 -0600

    Separated contents of login_as_user to django_su::utils.py::su_in to allow for suing in from python

--- django_su/utils.py
+++ django_su/utils.py
@@ -5,7 +5,43 @@ from collections.abc import Callable
 
 from django.conf import settings
 from django.utils.module_loading import import_string
+from django.contrib.auth import (
+    BACKEND_SESSION_KEY,
+    SESSION_KEY,
+    authenticate,
+    login,
+)
 
+def su_in(request, user_id):
+    '''
+    Returns: a User Object or None
+    '''
+    if not request.user.has_perm("auth.change_user"):
+        return None
+
+    userobj = authenticate(request=request, su=True, user_id=user_id)
+    if not userobj:
+        return None
+
+    exit_users_pk = request.session.get("exit_users_pk", default=[])
+    exit_users_pk.append(
+        (request.session[SESSION_KEY], request.session[BACKEND_SESSION_KEY])
+    )
+
+    maintain_last_login = hasattr(userobj, "last_login")
+    if maintain_last_login:
+        last_login = userobj.last_login
+
+    try:
+        if not custom_login_action(request, userobj):
+            login(request, userobj)
+        request.session["exit_users_pk"] = exit_users_pk
+    finally:
+        if maintain_last_login:
+            userobj.last_login = last_login
+            userobj.save(update_fields=["last_login"])
+
+        return userobj
 
 def su_login_callback(user):
     if hasattr(settings, "SU_LOGIN"):
--- django_su/views.py
+++ django_su/views.py
@@ -4,9 +4,6 @@ import warnings
 
 from django.conf import settings
 from django.contrib.auth import (
-    BACKEND_SESSION_KEY,
-    SESSION_KEY,
-    authenticate,
     get_user_model,
     login,
 )
@@ -17,7 +14,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_http_methods
 
 from .forms import UserSuForm
-from .utils import custom_login_action, su_login_callback
+from .utils import custom_login_action, su_login_callback, su_in
 
 
 User = get_user_model()
@@ -27,27 +24,8 @@ User = get_user_model()
 @require_http_methods(["POST"])
 @user_passes_test(su_login_callback)
 def login_as_user(request, user_id):
-    userobj = authenticate(request=request, su=True, user_id=user_id)
-    if not userobj:
+    if not su_in(request, user_id):
         raise Http404("User not found")
-
-    exit_users_pk = request.session.get("exit_users_pk", default=[])
-    exit_users_pk.append(
-        (request.session[SESSION_KEY], request.session[BACKEND_SESSION_KEY])
-    )
-
-    maintain_last_login = hasattr(userobj, "last_login")
-    if maintain_last_login:
-        last_login = userobj.last_login
-
-    try:
-        if not custom_login_action(request, userobj):
-            login(request, userobj)
-        request.session["exit_users_pk"] = exit_users_pk
-    finally:
-        if maintain_last_login:
-            userobj.last_login = last_login
-            userobj.save(update_fields=["last_login"])
 
     if hasattr(settings, "SU_REDIRECT_LOGIN"):
         warnings.warn(
